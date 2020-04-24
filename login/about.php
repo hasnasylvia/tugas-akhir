5b005b9dfcd1e1f922802a226ad6d14f9410153e
hasnasylvia <53207279+hasnasylvia@users.noreply.github.com>
2020-04-25 04:05:34 +0700

    Add files via upload

---
+++ login/about.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        <title>About</title>
+        <link rel="stylesheet" href="http://localhost/covid/asset/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="http://localhost/covid/asset/fontawesome/css/all.min.css">
+    </head>
+    <body>
+    <div class="container">
+    <div class="row mt-4">
+        <div class="col md-7">
+            <h2>About</h2>
+            <hr class="bg-light">
+                <table class="table table-bordered">
+                    <tr>
+                        <td><strong>Nama</strong></td>
+                        <td>Hasna Sylvia Maharani</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Kelas</strong></td>
+                        <td>X RPL 4</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Jurusan</strong></td>
+                        <td>RPL</td>
+                    </tr>
+                    <tr>
+                        <td><strong>Sekolah</strong></td>
+                        <td>SMK Telkom Malang</td>
+                    </tr>
+                    <tr>
+                        <td class="text-right" colspan="2">
+                            <a href="index.php" class="btn btn-success">
+                                <i class=" fa fa-angle-double-left"></i>Kembali
+                            </a>
+                        </td>
+                    </tr>
+                </table>
+        </div>
+    </div>
+</div>
+    </body>
+</html>
